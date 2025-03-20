63e7e1309d86abdb65e5e11eeee7d69f608f65b4
TinyFox <106698391+Ace-Kyle@users.noreply.github.com>
2025-03-20 14:16:11 +0700

    feat: Get name of Class type

--- src/model/ClassType.java
+++ src/model/ClassType.java
@@ -1,11 +1,32 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ClassType {
+    public ClassType(int classId) {
+    }
+
     enum CLASS_TYPE {
         ATTACKER,
         RUNNER,
         DEFENDER,
+        UNKNOWN,
     }
-    int id;
-    String name;
+    //in original data, its name is chara_role instead of chara_class
+    //it may cause some confuse to ElementType class
+    @JsonProperty("role_id")
+    private int id;
+    private String name;
+
+    public String getName() {
+        //example data: Attacker(s), Defender(s), Runner(s)
+        if (this.name.toUpperCase().contains(CLASS_TYPE.ATTACKER.toString())) return CLASS_TYPE.ATTACKER.toString();
+        if (this.name.toUpperCase().contains(CLASS_TYPE.RUNNER.toString())) return CLASS_TYPE.RUNNER.toString();
+        if (this.name.toUpperCase().contains(CLASS_TYPE.DEFENDER.toString())) return CLASS_TYPE.DEFENDER.toString();
+
+        return CLASS_TYPE.UNKNOWN.toString();
+    }
+    private String nameWithoutLeftover(){
+        return this.name.replaceAll("\\(.*\\)", "");
+    }
 }
