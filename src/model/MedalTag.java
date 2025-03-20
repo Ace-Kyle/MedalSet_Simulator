5c1651b3f3e9f73e2f7009ebbc47857b8deec0e3
TinyFox <106698391+Ace-Kyle@users.noreply.github.com>
2025-03-20 14:34:22 +0700

    refactor: Simply some field of MedalTag
    - medal_Tag_id = id
    - set2_ability_id = effect_pair
    - set3_ability_id = effect_trio

--- src/model/MedalTag.java
+++ src/model/MedalTag.java
@@ -1,25 +1,27 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MedalTag {
-    public int typeid;
-    private int medal_tag_id;
+
+    @JsonProperty("medal_tag_id")
+    private int id;
     private String name;
     private int tag_category;
-    private int set2_ability_id;
-    private int set3_ability_id;
+
+    @JsonProperty("set2_ability_id")
+    private int effect_pair; //Pair-effect
+    @JsonProperty("set3_ability_id")
+    private int effect_trio; //Trio-effect
     private int sort_id;
 
     public MedalTag(){}
 
     // Getters and setters
-    public int getMedal_tag_id() { return medal_tag_id; }
-    public void setMedal_tag_id(int medal_tag_id) { this.medal_tag_id = medal_tag_id; }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
@@ -27,11 +29,11 @@ public class MedalTag {
     public int getTag_category() { return tag_category; }
     public void setTag_category(int tag_category) { this.tag_category = tag_category; }
 
-    public int getSet2_ability_id() { return set2_ability_id; }
-    public void setSet2_ability_id(int set2_ability_id) { this.set2_ability_id = set2_ability_id; }
+    public int getSet2_ability_id() { return effect_pair; }
+    public void setSet2_ability_id(int set2_ability_id) { this.effect_pair = set2_ability_id; }
 
-    public int getSet3_ability_id() { return set3_ability_id; }
-    public void setSet3_ability_id(int set3_ability_id) { this.set3_ability_id = set3_ability_id; }
+    public int getSet3_ability_id() { return effect_trio; }
+    public void setSet3_ability_id(int set3_ability_id) { this.effect_trio = set3_ability_id; }
 
     public int getSort_id() { return sort_id; }
     public void setSort_id(int sort_id) { this.sort_id = sort_id; }
@@ -39,8 +41,8 @@ public class MedalTag {
 
     public String getAffectByNumberCombineTag(int number) {
         return switch (number){
-            case 2 -> new Ability(this.set2_ability_id).getAffects().toString();
-            case 3 -> new Ability(this.set3_ability_id).getAffects().toString();
+            case 2 -> new Ability(this.effect_pair).getAffects().toString();
+            case 3 -> new Ability(this.effect_trio).getAffects().toString();
             default -> "";
         };
     }
