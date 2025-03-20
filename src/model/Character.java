28c02595c77eb2e7ee3fb406f16de5db9712c2c5
TinyFox <106698391+Ace-Kyle@users.noreply.github.com>
2025-03-20 13:52:03 +0700

    feat: Complete isPlayableCharacter method
    Logic bases on:
    Reality analysic process

--- src/model/Character.java
+++ src/model/Character.java
@@ -1,31 +1,67 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Character {
-    private int chara_id;
+    @JsonProperty("chara_id")
+    private int id;
     private String name;
     private String name_en;
     private String nickname;
+    private String filename;
+
+    @JsonProperty("class_id")
+    private int element_id;
+    @JsonProperty("role_id")
+    private int class_id;
+
     private int person_id;
-    private int base_rarity;
-    private int max_rarity;
+    private int sub_person_id;
+    private int change_role_id1;    //class 1
+    private int change_role_id2;    //class 2
+    private int is_change_class;    //element
+
+    private boolean is_legend;
+    @JsonProperty("base_rarity")
+    private int star;
     private List<Integer> tag_ids;
     private int skill1_id;
     private int skill2_id;
-    private Integer abillity0_id;
+    private int abillity0_id;
     private int abillity1_id;
     private int abillity2_id;
+    private int team_skill_id;
+
+    private int character_info;
+    @JsonProperty("chara_type")
+    private int character_type;
+
     // Additional fields omitted for brevity, add as needed
 
     public Character(){}
 
+    //helper
+    private boolean isPlayableCharacter(){
+        return !(
+                this.name.contains("?") ||
+                this.id > 400007000 ||
+                this.character_info >=1);
+    }
+    private boolean isDoubleCharacer(){
+        return  this.character_info >=1;
+    }
+
     // Getters and setters
-    public int getChara_id() { return chara_id; }
-    public void setChara_id(int chara_id) { this.chara_id = chara_id; }
+    public String getFilename() { return filename;}
+    public int getClass_id(){ return class_id; }
+    public int getElement_id(){ return element_id; }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
@@ -39,11 +75,8 @@ public class Character {
     public int getPerson_id() { return person_id; }
     public void setPerson_id(int person_id) { this.person_id = person_id; }
 
-    public int getBase_rarity() { return base_rarity; }
-    public void setBase_rarity(int base_rarity) { this.base_rarity = base_rarity; }
-
-    public int getMax_rarity() { return max_rarity; }
-    public void setMax_rarity(int max_rarity) { this.max_rarity = max_rarity; }
+    public int getBase_rarity() { return star; }
+    public void setBase_rarity(int base_rarity) { this.star = base_rarity; }
 
     public List<Integer> getTag_ids() { return tag_ids; }
     public void setTag_ids(List<Integer> tag_ids) { this.tag_ids = tag_ids; }
@@ -62,4 +95,9 @@ public class Character {
 
     public int getAbillity2_id() { return abillity2_id; }
     public void setAbillity2_id(int abillity2_id) { this.abillity2_id = abillity2_id; }
+    public int getTeam_skill_id() { return team_skill_id; }
+    public boolean getIsLegend() { return is_legend; }
+    public int getStar() { return star; }
+
 }
+//chara_id of "Roger" = 400000581
