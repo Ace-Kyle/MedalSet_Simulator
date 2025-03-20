e9cfd1f3abaca7517dd40b5c2957e3110fe00837
TinyFox <106698391+Ace-Kyle@users.noreply.github.com>
2025-03-20 13:39:08 +0700

    feat: Define character rarity based on their property
    All conditions are self-explored process in reality because it cannot know logical code of game now

--- src/model/Rarity.java
+++ src/model/Rarity.java
@@ -7,6 +7,11 @@ public class Rarity {
         STEP_UP,
         FREE,
     }
-    int id;
-    String name;
+    //define character type (rarity) based on their property
+    public String getRarityName(Character character){
+        if (character.getTeam_skill_id() > 110) return RarityType.EX.toString();
+        if (character.getIsLegend()) return RarityType.BF.toString();
+        if (character.getStar() != 30) return RarityType.STEP_UP.toString();
+        return RarityType.FREE.toString();
+    }
 }
